c87faa13e116ed33d18a31ef1627c8f26d8d15fe
emiliedagostino <edagosti@calpoly.edu>
2021-05-16 22:44:38 -0700

    Make Phrase-Function

--- R/make_phrase.R
+++ R/make_phrase.R
@@ -13,17 +13,23 @@
 #' @import glue
 #' @import dplyr
 #' @import purrr
+#' @import english
 #'
 #' @export
-
-
-
 make_phrase <- function(num, num_word, item, verb, adjective, location){
 
-  verb <- str_replace_na(verb, "")
-
-  #????
-
-
+  verb <- stringr::str_replace_na(verb, "")
+  adjective <- stringr::str_replace_na(adjective, "")
+  location <- stringr::str_replace_na(location, "")
+  item = case_when(
+    num==1 ~as.character(item),
+    TRUE~ as.character(pluralize_gift(item))
+  )
+  num_word = case_when(
+    num==1~ "a",
+    TRUE~as.character(english::english(num))
+  )
+  glue<- glue("{num_word} {adjective} {item} {verb} {location}")
+  together<-str_squish(glue)
+  return(together)
 }
-
